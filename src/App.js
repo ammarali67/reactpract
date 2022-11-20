500bc07e6b25e72dd616eef0e385a2ea7be45661
Ammar Ali <ammarali67123@gmail.com>
2022-11-20 15:03:08 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const [employeeList, setEmployeeList] = useState([]); 
   
   const addEmployee = () => {
-    Axios.post('https://reactapp-new.herokuapp.com/create',{
+    Axios.post('/create',{
       name: name,
       age: age,
       position: position,
@@ -36,13 +36,13 @@ function App() {
   };
 
   const getEmployees = ()=>{
-    Axios.get("https://reactapp-new.herokuapp.com/employees").then((response)=>{
+    Axios.get("/employees").then((response)=>{
       setEmployeeList(response.data);
     });
   };
 
   const updateEmployeeWage = (id) => {
-    Axios.put("https://reactapp-new.herokuapp.com/update", { wage: newWage, id: id }).then(
+    Axios.put("/update", { wage: newWage, id: id }).then(
       (response) => {
         setEmployeeList(
           employeeList.map((val) => {
@@ -63,7 +63,7 @@ function App() {
   };
 
   const deleteEmployee = (id) => {
-    Axios.delete(`https://reactapp-new.herokuapp.com/delete/${id}`).then((response) => {
+    Axios.delete(`/delete/${id}`).then((response) => {
       setEmployeeList(
         employeeList.filter((val) => {
           return val.id != id;
